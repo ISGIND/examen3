8099d1e249c95f8026edecb97d7c6c3de4c096d4
Raul Sanchez Olvera <raul.sanchez.92@ciencias.unam.mx>
2019-09-27 14:11:53 -0500

    PedidoResponse

--- examen3-pedidos/src/main/java/com/everis/pedidos/response/PedidoResponse.java
+++ examen3-pedidos/src/main/java/com/everis/pedidos/response/PedidoResponse.java
@@ -1,5 +1,24 @@
 package com.everis.pedidos.response;
 
-public class PedidoResponse {
+import com.everis.pedidos.model.Pedido;
 
+public class PedidoResponse {
+	
+	private boolean successful;
+	private Pedido pedido;
+
+	public boolean isSuccessful() {
+		return successful;
+	}
+	public void setSuccessful(boolean successful) {
+		this.successful = successful;
+	}
+	public Pedido getPedido() {
+		return pedido;
+	}
+	public void setPedido(Pedido pedido) {
+		this.pedido = pedido;
+	}
+
+	
 }
