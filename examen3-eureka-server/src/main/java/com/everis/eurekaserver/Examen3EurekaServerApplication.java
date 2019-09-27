5101e328f995c3729eb551ad1792ca9e87ee5c11
Ivan <ivansanchez.g13@gmai.com>
2019-09-27 11:50:14 -0500

    Se agregan los proyectos pedidos y eureka server

---
+++ examen3-eureka-server/src/main/java/com/everis/eurekaserver/Examen3EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.everis.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Examen3EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Examen3EurekaServerApplication.class, args);
+	}
+
+}
---
+++ examen3-pedidos/src/main/java/com/everis/pedidos/Examen3PedidosApplication.java
@@ -0,0 +1,13 @@
+package com.everis.pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Examen3PedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Examen3PedidosApplication.class, args);
+	}
+
+}
