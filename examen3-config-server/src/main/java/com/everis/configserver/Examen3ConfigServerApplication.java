dcd9da3f3089f7b3fca029a29624ba830d9a04a8
francisco <franzxav1852@gmail.com>
2019-09-27 12:36:32 -0500

    actualizacion del config server

--- examen3-config-server/src/main/java/com/everis/configserver/Examen3ConfigServerApplication.java
+++ examen3-config-server/src/main/java/com/everis/configserver/Examen3ConfigServerApplication.java
@@ -2,7 +2,8 @@ package com.everis.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.config.server.EnableConfigServer;
+@EnableConfigServer
 @SpringBootApplication
 public class Examen3ConfigServerApplication {
 
