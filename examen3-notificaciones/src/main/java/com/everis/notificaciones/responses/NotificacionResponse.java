2e4207ce7f9c5b012ff97340927384d429718136
francisco <franzxav1852@gmail.com>
2019-09-27 15:53:44 -0500

    Notificacion Response
    de NotificacionesController

--- examen3-notificaciones/src/main/java/com/everis/notificaciones/responses/NotificacionResponse.java
+++ examen3-notificaciones/src/main/java/com/everis/notificaciones/responses/NotificacionResponse.java
@@ -1,6 +1,46 @@
 package com.everis.notificaciones.responses;
 
+import java.math.BigDecimal;
+
+import com.everis.notificaciones.model.Pedido;
+
 public class NotificacionResponse {
 	
+	private boolean successful;
+	private String tipoMensaje; 
+	private String message;
+	private Pedido pedido;
+	private BigDecimal port;	
+	
+	public boolean isSuccessful() {
+		return successful;
+	}
+	public void setSuccessful(boolean successful) {
+		this.successful = successful;
+	}
+	public String getTipoMensaje() {
+		return tipoMensaje;
+	}
+	public void setTipoMensaje(String tipoMensaje) {
+		this.tipoMensaje = tipoMensaje;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public Pedido getPedido() {
+		return pedido;
+	}
+	public void setPedido(Pedido pedido) {
+		this.pedido = pedido;
+	}
+	public BigDecimal getPort() {
+		return port;
+	}
+	public void setPort(BigDecimal port) {
+		this.port = port;
+	}
 	
 }
